ae99c0ae3d973a1f65b7e30d7e51ab51b7a35963
earth2travis <travis.mccutcheon@gmail.com>
2024-03-29 18:28:35 -0500

    Add routes and welcome image

---
+++ api/items.tsx
@@ -0,0 +1,36 @@
+import { Button, Frog } from 'frog';
+
+export const app = new Frog();
+
+app.frame('/', c => {
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#ff3864',
+          justifyContent: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'black',
+            display: 'flex',
+            alignItems: 'center',
+            fontSize: 60,
+          }}
+        >
+          Items
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Link href="https://www.raidguild.org/">RaidGuild</Button.Link>,
+      <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
